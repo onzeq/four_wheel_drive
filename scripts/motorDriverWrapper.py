9c539470f3b76efe02e939e4bb7a9af6978c5e78
onzeq <onzeq.v@gmail.com>
2021-12-10 20:25:06 +0100

    changed to OOP

---
+++ scripts/motorDriverWrapper.py
@@ -0,0 +1,18 @@
+import rospy
+from driver_scripts.drives import Drives
+from std_msgs.msg import Int32
+from std_srvs.srv import Trigger
+
+class MotorDriver:
+    def __init__(self) -> None:
+        self.drives = Drives
+        rospy.Service("stop_motor", Trigger, self.callback_stop)
+        
+
+
+    def callback_stop(self, req):
+        self.drives.stop()
+        return {"success": True, "message": "Motors have been stopped"}
+
+    def stop(self):
+        self.drives.stop()
