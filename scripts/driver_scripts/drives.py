2dd5853a50176287a1a2907061963b0a6fffe7fd
onzeq <onzeq.v@gmail.com>
2021-12-11 16:41:52 +0100

    added constants file

--- scripts/driver_scripts/drives.py
+++ scripts/driver_scripts/drives.py
@@ -1,22 +1,6 @@
 import time
-from enum import Enum
 from adafruit_motorkit import MotorKit
 import board
-
-
-
-class Commands(Enum):
-    FORWARD         = 0
-    BACKWARD        = 2
-    LEFT_ANGULAR    = 4
-    RIGHT_ANGULAR   = 8
-    TURN_LEFT       = 16
-    TURN_RIGHT      = 32
-    STOP            = 64
-
-
-SPEED_VAL:float = 0.5
-
 
 
 class Drives:
